3b5f31755981bd904ecba54404aca3e8d4fe5644
github username <github email address>
2020-02-11 14:08:31 -0700

    Added the roasters to the dropdown form, as well as added a quantity option and an adding to cart button

--- app.js
+++ app.js
@@ -2,9 +2,21 @@ fetch('http://localhost:3000/coffees')
 .then(response => response.json())
 .then(coffees => {
     coffees.map(coffee => {
-        console.log(coffee)
         let coffeeName = document.createElement('h3')
         coffeeName.innerText = `Coffee Roaster: ${coffee.coffee_roaster.name}, Coffee Blend: ${coffee.blend.name}`
         document.body.append(coffeeName)
+    })
+})
+
+const select = document.querySelector('select')
+fetch("http://localhost:3000/coffee_roasters")
+.then( response => response.json())
+.then(roasterArray => {
+    roasterArray.forEach(roaster => {
+    let option = document.createElement('option')
+            option.innerText = roaster.name
+            option.value = roaster.id
+
+            select.append(option)
     })
 })
